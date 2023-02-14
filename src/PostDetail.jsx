f51c5ceb69b7cf67ae1cc7fac62823710a5bdd3d
Matheus Massari <massari.matheus@gmail.com>
2023-02-14 18:35:01 -0300

    useMutation example

--- src/PostDetail.jsx
+++ src/PostDetail.jsx
@@ -1,4 +1,4 @@
-import { useQuery } from "react-query";
+import { useMutation, useQuery } from "react-query";
 
 async function fetchComments(postId) {
     const response = await fetch(
@@ -25,15 +25,41 @@ async function updatePost(postId) {
 
 export function PostDetail({ post }) {
     // replace with useQuery
-    console.log(post);
+
     const { data, isLoading, error } = useQuery(["comments", post.id], () =>
         fetchComments(post.id)
     );
 
+    const deleteMutation = useMutation((postId) => deletePost(postId));
+    const updateMutation = useMutation((postId) => updatePost(postId));
+
     return (
         <>
             <h3 style={{ color: "blue" }}>{post.title}</h3>
-            <button>Delete</button> <button>Update title</button>
+            <button onClick={() => deleteMutation.mutate(post.id)}>
+                Delete
+            </button>{" "}
+            {deleteMutation.isError && (
+                <p style={{ color: "red" }}> Error deleting the post.</p>
+            )}
+            {deleteMutation.isLoading && (
+                <p style={{ color: "purple" }}> Loading ... </p>
+            )}
+            {deleteMutation.isSuccess && (
+                <p style={{ color: "green" }}> Successful request ! </p>
+            )}
+            <button onClick={() => updateMutation.mutate(post.id)}>
+                Update title
+            </button>
+            {updateMutation.isError && (
+                <p style={{ color: "red" }}> Error to update!</p>
+            )}
+            {updateMutation.isLoading && (
+                <p style={{ color: "purple" }}> Update loading...</p>
+            )}
+            {updateMutation.isSuccess && (
+                <p style={{ color: "green" }}> Update complete!</p>
+            )}
             <p>{post.body}</p>
             <h4>Comments</h4>
             {isLoading && (
--- src/Posts.jsx
+++ src/Posts.jsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { useQuery } from "react-query";
+import { useEffect, useState } from "react";
+import { useQuery, useQueryClient } from "react-query";
 
 import { PostDetail } from "./PostDetail";
 
@@ -16,12 +16,25 @@ export function Posts() {
     const [currentPage, setCurrentPage] = useState(1);
     const [selectedPost, setSelectedPost] = useState(null);
 
-    // replace with useQuery
+    const queryClient = useQueryClient();
+
+    // Pre-fetching data for posts
+    useEffect(() => {
+        if (currentPage < maxPostPage) {
+            const nextPage = currentPage + 1;
+            queryClient.prefetchQuery(["posts", nextPage], () =>
+                fetchPosts(nextPage)
+            );
+        }
+    }, [currentPage, queryClient]);
+    //
+
     const { data, isError, error, isLoading } = useQuery(
         ["posts", currentPage],
         () => fetchPosts(currentPage),
         {
             staleTime: 2000,
+            keepPreviousData: true,
         }
     );
     if (isLoading) {
